3de25e83e7b8b650875581fea65558e11c9804e6
harshalmore31 <harshalmore2468@gmail.com>
2026-01-26 20:05:24 +0530

    updates !

--- memori/embeddings/__init__.py
+++ memori/embeddings/__init__.py
@@ -1,3 +1,11 @@
+"""
+Embeddings utilities.
+
+The public entrypoints are:
+- embed_texts
+- format_embedding_for_db
+"""
+
 from memori.embeddings._api import embed_texts
 from memori.embeddings._format import format_embedding_for_db
 from memori.embeddings._tei import TEI
