018a1efcc1380148898cfc1319f9b2db5a26f094
Ryan Kruse <rpkruse1@gmail.com>
2026-04-23 16:06:18 -0500

    Updated files to match main

--- memori-ts/src/native/build.js
+++ memori-ts/src/native/build.js
@@ -2,12 +2,12 @@ const { execSync } = require('child_process');
 
 const env = { ...process.env };
 
-// NAPI-RS forces static C-Runtime on Windows by default. 
-// We must override it to dynamic (-crt-static) so that ort-sys and tokenizers 
+// NAPI-RS forces static C-Runtime on Windows by default.
+// We must override it to dynamic (-crt-static) so that ort-sys and tokenizers
 // can compile together without LNK2038 linker collisions.
 if (process.platform === 'win32') {
-  env.RUSTFLAGS = env.RUSTFLAGS 
-    ? `${env.RUSTFLAGS} -C target-feature=-crt-static` 
+  env.RUSTFLAGS = env.RUSTFLAGS
+    ? `${env.RUSTFLAGS} -C target-feature=-crt-static`
     : '-C target-feature=-crt-static';
   console.log('Detected Windows: Injecting dynamic C-Runtime RUSTFLAGS...');
 }
